e95474566ab811086da739af957b27e0cb9752e7
JackLin <chai520a@gmail.com>
2024-12-09 21:46:29 +0800

    Update tools

--- make_build.sh
+++ make_build.sh
@@ -6,9 +6,10 @@ ubuntu_version="22.04"
 
 make_TensorRT()
 {
-    # cd TensorRT
-    # ./docker/build.sh --file docker/ubuntu-${ubuntu_version}.Dockerfile --tag tensorrt-ubuntu${ubuntu_version}-cuda${cuda_version}
-    # cd ..
+    cd 3rdparty/TensorRT
+    ./docker/build.sh --file docker/ubuntu-${ubuntu_version}.Dockerfile --tag tensorrt-ubuntu${ubuntu_version}-cuda${cuda_version}:${arch}
+    cd ../../
+
     docker run --rm -it -v `pwd`:/workspace tensorrt-ubuntu22.04-cuda12.6 bash -c \
     "mkdir build_${arch} && cd build_${arch} && mkdir ${engine} &&
     cp -rf /TensorRT*/include /workspace/build_${arch}/${engine} &&
@@ -17,4 +18,16 @@ make_TensorRT()
     exit
     "
 }
-make_${engine}
+
+make_opencv()
+{
+    cd docker
+    docker build -t opencv -f build_opencv.dockerfile .
+    cd ..
+    docker run --rm -it -v `pwd`:/workspace opencv bash -c \
+    "cp -rf /opencv /workspace/build_${arch} && \
+    exit"
+
+}
+# make_${engine}
+make_opencv
